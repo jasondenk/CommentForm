06914d0da00f8d7c59bbd564cbc829523b47e30e
Jason Denk <jdenk9@gmail.com>
2017-02-07 14:30:43 -0500

    Added properties to my CommentFormModel class and completed it

--- CommentForm/Models/CommentFormModel.cs
+++ CommentForm/Models/CommentFormModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace CommentForm.Models
 {
     public class CommentFormModel
     {
+        //Our ID keeps track of each instance
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public int Priority { get; set; }
     }
 }
